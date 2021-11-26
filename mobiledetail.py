c47bb49eda3376261427ffb1eeca19998550cf29
Diya Patel <85157082+diyapateldotcom@users.noreply.github.com>
2021-11-26 21:53:39 +0530

    Update mobiledetail.py

--- mobiledetail.py
+++ mobiledetail.py
@@ -1,7 +1,7 @@
 import phonenumbers
 from phonenumbers import carrier,geocoder,timezone
 
-mobileNo=phonenumbers.parse("+91 99990 55525")
+mobileNo=phonenumbers.parse("+91 99990 55525")                        #write phone number
 print(timezone.time_zones_for_number(mobileNo))
 print(carrier.name_for_number(mobileNo,"en"))
 print(geocoder.description_for_number(mobileNo,"en"))
