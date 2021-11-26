f033758949c56fbe1934b05885580cc061faf95f
Diya Patel <85157082+diyapateldotcom@users.noreply.github.com>
2021-11-26 21:52:19 +0530

    Mobile Tracker
    This is a program that represent the details of phone number that you typed.
    You can easily found your company detail of SIM.

---
+++ mobiledetail.py
@@ -0,0 +1,9 @@
+import phonenumbers
+from phonenumbers import carrier,geocoder,timezone
+
+mobileNo=phonenumbers.parse("+91 99990 55525")
+print(timezone.time_zones_for_number(mobileNo))
+print(carrier.name_for_number(mobileNo,"en"))
+print(geocoder.description_for_number(mobileNo,"en"))
+print("Valid Mobile Number:",phonenumbers.is_valid_number(mobileNo))
+print("Checking_possibility of Number:",phonenumbers.is_possible_number(mobileNo))
